454cfae52b580d90afef61a5f84b05ca72054de8
SingularBotTest <37448914+SingularMaker@users.noreply.github.com>
2018-08-07 20:41:03 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -789,7 +789,7 @@ namespace singularbot {
      *	Read FT Light Sensor
 	 */
     //% weight=50 blockId=getFTLight block="Get FT Light Status %digital_pin"
-    export function getFTLight(digital_pin: number): number {
+    export function getFTLight(digital_pin: number):  boolean {
         let s = pins.digitalReadPin(digital_pin);
              s = ~s;
 	    return s;
