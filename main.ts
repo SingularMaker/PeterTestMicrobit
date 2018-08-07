8b1f04836d295fdb3220bbd0f863c13d56d5d43e
SingularBotTest <37448914+SingularMaker@users.noreply.github.com>
2018-08-07 21:12:38 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -791,6 +791,6 @@ namespace singularbot {
     //% weight=50 blockId=getFTLight block="Get FT Light Status %digital_pin"
     export function getFTLight(digital_pin: number):  number{
         let s = pins.digitalReadPin(DigitalPin.P2);
-        basic.showNumber(s);
+        serial.writeBuffer(s);;
 	}	
 }
